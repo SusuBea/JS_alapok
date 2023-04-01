71396abc5603acab18c092f9385fb1e07182aa64
SusuBea <suhajdabea@gmail.com>
2023-04-01 11:24:38 +0200

    megoldás JQueryvel

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
-});
+// window.addEventListener("load", function () {
+//     elemekElerese1();
+//     elemekElerese2();
+//     elemekElerese3();
+// });
 
-function elemekElerese1() {
-    /**Ide jön az első feladat */
-}
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
-}
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
-}
+// function elemekElerese1() {
+//     /**Ide jön az első feladat */
+// }
+// function elemekElerese1() {
+//     /**Ide jön az 2.  feladat */
+// }
+// function elemekElerese1() {
+//     /**Ide jön az 3. feladat */
+// }
+
+$(function(){
+    
+ 
+  });
